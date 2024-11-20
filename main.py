4c43ac5c66587fac1aa0f0f64b8437a18d18d91c
Angel John Oselotos <a.i.oselotos@gmail.com>
2024-11-20 11:32:59 +0200

    minor

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.write("## This app is a presenation of several models presented by Mike Saint-Antoine")
 st.page_link("https://www.youtube.com/playlist?list=PLWVKUEZ25V94kdT2Lh97KqB9MoLV9ZzmU", label=" link to Saint-Antoine's youtube videos")
-st.write("### please select a model from the side bar and explore its concepts.")
+st.write("#### please select a model from the side bar and explore its concepts.")
 
